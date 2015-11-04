ca6425b049f562172685ceeafc034744da94b21a
Muh Muhten <muh.muhten@gmail.com>
2015-11-03 22:46:37 -0500

    vrcgen now takes lua config directly

---
+++ over/etc/rc/bogonet.lua
@@ -0,0 +1,24 @@
+rc:merge {
+  notify = {
+    type = "longrun",
+    producer_for = "adjust",
+    run = util.realign [[
+    #!/usr/bin/execlineb -P
+    chroot /mnt
+    curl -NsS --unix-socket /var/run/docker.sock
+    0/v1.20/containers/json
+    0/v1.20/events
+    ]]
+  },
+
+  adjust = {
+    type = "longrun",
+    consumer_for = "notify",
+    run = util.file_slurp "bogonet/adjust.lua",
+    data = {
+      ["sha256.lua"] = util.file_slurp "bogonet/sha256/sha256.lua"
+    }
+  },
+
+  [3] = { contents = "notify" },
+}
